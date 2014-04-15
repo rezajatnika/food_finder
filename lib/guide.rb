c7b4004270451d9313c552c2db85dcbeb996fd62
Reza Jatnika <rezajatnika@gmail.com>
2014-04-15 15:21:46 +0700

    Finish sort function

--- lib/guide.rb
+++ lib/guide.rb
@@ -53,7 +53,7 @@ class Guide
   def do_action(action, args=[])
     case action
     when 'list'
-      list
+      list(args)
     when 'find'
       keyword = args.shift
       find(keyword)
@@ -92,10 +92,26 @@ class Guide
     end
   end
   
-  def list
+  def list(args=[])
+    sort_order = args.shift
+    sort_order = args.shift if sort_order == 'by'
+    sort_order = "name" unless ['name', 'cuisine', 'price'].include?(sort_order)
+    
     output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
+    
+    restaurants.sort! do |r1, r2|
+      case sort_order
+      when 'name'
+        r1.name.downcase <=> r2.name.downcase
+      when 'cuisine'
+        r1.cuisine.downcase <=> r2.cuisine.downcase
+      when 'price'
+        r1.price.to_i <=> r2.price.to_i
+      end
+    end
     output_restaurant_table(restaurants)
+    puts "Sort using: 'list by #{sort_order}'\n\n"
   end
   
   def introduction
@@ -114,8 +130,8 @@ class Guide
   
   def output_restaurant_table(restaurants=[])
     print " " + "Name".titleize.ljust(30)
-    print " " + "Name".titleize.ljust(20)
-    print " " + "Name".rjust(6) + "\n"
+    print " " + "Cuisine".titleize.ljust(20)
+    print " " + "Price".rjust(6) + "\n"
     puts "-" * 60
     restaurants.each do |rest|
       line = " " << rest.name.titleize.ljust(30)
